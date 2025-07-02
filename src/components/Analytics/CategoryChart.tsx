1161080ee051eef9b29ecb5387a549299cb28b5e
Dante O. Cuales, Jr. <danteocualesjr@gmail.com>
2025-07-02 10:30:04 +0800

    Updated CategoryChart.tsx

--- src/components/Analytics/CategoryChart.tsx
+++ src/components/Analytics/CategoryChart.tsx
@@ -12,6 +12,7 @@ interface CategoryChartProps {
 const CategoryChart: React.FC<CategoryChartProps> = ({ data }) => {
   
   const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }: any) => {
+    
     if (percent < 0.05) return null; // Don't show labels for slices smaller than 5%
     
     const RADIAN = Math.PI / 180;
