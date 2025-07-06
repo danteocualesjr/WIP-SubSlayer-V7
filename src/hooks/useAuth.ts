71bb397119a38328a3d55a6df28825deb2f677b9
Dante O. Cuales, Jr. <danteocualesjr@gmail.com>
2025-07-07 01:01:19 +0800

    Updated useAuth.ts

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -12,7 +12,7 @@ export function useAuth() {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: window.location.origin
+          redirectTo: `${window.location.origin}/`
         }
       });
       return { data, error };
